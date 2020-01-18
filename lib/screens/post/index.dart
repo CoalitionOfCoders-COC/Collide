166802e420c6151663e86016863da03b88a9c5b1
amreesalmonte <amreesalmonte@gmail.com>
2020-01-18 15:25:34 -0700

    added tabBar

--- lib/screens/post/index.dart
+++ lib/screens/post/index.dart
@@ -8,39 +8,62 @@ class PostIndex extends StatefulWidget {
   _PostIndex createState() => _PostIndex();
 }
 
-class _PostIndex extends State<PostIndex>{
+class _PostIndex extends State<PostIndex> {
+  // tabs that will appear need
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      backgroundColor: Colors.brown[300], // Change once final color swatch chosen,
-      appBar: AppBar(
-        title: Text(
-          'Explore',
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 6,
+      child: Scaffold(
+          backgroundColor:
+              Colors.brown[300], // Change once final color swatch chosen,
+          // app bar with icons and title
+          appBar: AppBar(
+              elevation: 0.0,
+              title: Text(
+                'Explore',
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25,),
+              ),
+              backgroundColor:
+                  Colors.brown[300], // Change once final color swatch chosen
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(
+                    Icons.search,
+                    color: Colors.white,
+                  ),
+                  onPressed: null,
+                ), // search icon
+                IconButton(
+                  icon: Icon(
+                    Icons.settings,
+                    color: Colors.white,
+                  ),
+                  onPressed: null,
+                ), //gear icon
+              ],
+              // tab bar for navigation
+              bottom:TabBar(
+                isScrollable: true,
+                tabs: [
+                  Tab(text: 'Sample'),
+                  Tab(text: 'Sample'),
+                  Tab(text: 'Sample'),
+                  Tab(text: 'Sample'),
+                  Tab(text: 'Sample'),
+                  Tab(text: 'Sample'),
+                ],
+              ),
+            ),
+            body: ListView.builder(
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  child: Text("Welcome to Post Index!!!"),
+                );
+              },
+            )
         ),
-        backgroundColor: Colors.brown[300], // Change once final color swatch chosen
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.search,
-              color: Colors.white,),
-            onPressed: null,
-          ),// search icon
-          IconButton(
-            icon: Icon(
-              Icons.settings,
-              color: Colors.white,),
-            onPressed: null,
-          ), //gear icon
-        ]
-      ),
-      body: ListView.builder(
-        itemBuilder: (BuildContext context, int index){
-          return Card(
-            child: Text("Welcome to Post Index!!!"),
-          );
-        },
-      )
     );
-  }
-}
+  } // widget
+} // class _PostIndex

7baf910847c7432cd974ad6b722f59e6e837ab4a
amreesalmonte <amreesalmonte@gmail.com>
2020-01-18 13:56:46 -0700

    created app bar

--- lib/screens/post/index.dart
+++ lib/screens/post/index.dart
@@ -12,6 +12,28 @@ class _PostIndex extends State<PostIndex>{
   @override
   Widget build(BuildContext context){
     return Scaffold(
+      backgroundColor: Colors.brown[300], // Change once final color swatch chosen,
+      appBar: AppBar(
+        title: Text(
+          'Explore',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.brown[300], // Change once final color swatch chosen
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              color: Colors.white,),
+            onPressed: null,
+          ),// search icon
+          IconButton(
+            icon: Icon(
+              Icons.settings,
+              color: Colors.white,),
+            onPressed: null,
+          ), //gear icon
+        ]
+      ),
       body: ListView.builder(
         itemBuilder: (BuildContext context, int index){
           return Card(

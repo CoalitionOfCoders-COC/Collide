0348d5794166bb1c3f12d44ea43b5394d6d5e60f
amreesalmonte <amreesalmonte@gmail.com>
2020-01-18 16:33:51 -0700

    added partials folder

---
+++ lib/partials/cardposts.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CardPosts extends StatefulWidget {
+  @override
+  _CardPosts createState() => _CardPosts();
+}
+
+class _CardPosts extends State<CardPosts> {
+  @override
+  Widget build(BuildContext contex) {
+    return Text('text',);
+  }// widget build
+}// class CardPosts

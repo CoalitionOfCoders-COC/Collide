221a984d8cfea831c7391fee63861f8ea1092c67
amreesalmonte <amreesalmonte@gmail.com>
2020-01-18 23:28:40 -0700

    cleaned up font

--- lib/partials/cardposts.dart
+++ lib/partials/cardposts.dart
@@ -38,36 +38,37 @@ class _CardPosts extends State<CardPosts> {
             ),
             Container(
               height: 40,
-              child: Text(
-                "Title", 
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 25,
-                )),
+              child: Text("Title",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                  )),
             ),
             SizedBox(
                 height: 60,
                 child: Row(
                   children: <Widget>[
                     Container(
-                      margin: const EdgeInsets.only(left:18.0, bottom: 10.0),
+                      margin: const EdgeInsets.only(left: 18.0, bottom: 10.0),
                       width: 50.0,
                       height: 50.0,
                       decoration: new BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: new DecorationImage(
-                            fit: BoxFit.cover,
-                            image: new NetworkImage(
-                                'http://images.ctfassets.net/r7p9m4b1iqbp/NTJuSk4g0uaOwCGY8SwMq/228075a2bb20d88aff03466239628ed6/Natalie-Allen-12-Self-Portait-Japan.jpg?fit=fill&w=800&h=450')
-                          )
-                        ),
+                          shape: BoxShape.circle,
+                          image: new DecorationImage(
+                              fit: BoxFit.cover,
+                              image: new NetworkImage(
+                                  'http://images.ctfassets.net/r7p9m4b1iqbp/NTJuSk4g0uaOwCGY8SwMq/228075a2bb20d88aff03466239628ed6/Natalie-Allen-12-Self-Portait-Japan.jpg?fit=fill&w=800&h=450'))),
                     ),
-                        /*
-                        child: Image.network(
-                            'http://images.ctfassets.net/r7p9m4b1iqbp/NTJuSk4g0uaOwCGY8SwMq/228075a2bb20d88aff03466239628ed6/Natalie-Allen-12-Self-Portait-Japan.jpg?fit=fill&w=800&h=450')),*/
-                    Text("Author"),
-                    Text("Time")
+                    Container(
+                      margin: const EdgeInsets.only(left: 15.0),
+                      child: Text("by Author",
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17,
+                          )),
+                    )
                   ],
                 ))
           ],

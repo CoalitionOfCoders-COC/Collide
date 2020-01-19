f2741b8695303f6011592f11199af50755e67864
amreesalmonte <amreesalmonte@gmail.com>
2020-01-18 23:07:42 -0700

    fixed images

--- lib/partials/cardposts.dart
+++ lib/partials/cardposts.dart
@@ -19,24 +19,58 @@ class _CardPosts extends State<CardPosts> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: BoxDecoration(
-          color: Colors.white
-        ),
-        height: 250, // will change depending on image
-        margin: const EdgeInsets.all(10.0),
-        child: Column(children: <Widget>[
-          SizedBox(
-            height: 200,
-            child: Image.network('http://images.ctfassets.net/r7p9m4b1iqbp/NTJuSk4g0uaOwCGY8SwMq/228075a2bb20d88aff03466239628ed6/Natalie-Allen-12-Self-Portait-Japan.jpg?fit=fill&w=800&h=450')
-          ),
-          Text("Title"),
-          Text("Author")
-        ],)
-    );
+        decoration: ShapeDecoration(
+            color: Colors.white,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(17.0))),
+        margin: const EdgeInsets.only(
+            left: 15, right: 15, top: 10, bottom: 10), // margin of cards
+        child: Column(
+          children: <Widget>[
+            // posted image
+            ClipRRect(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(17.0),
+                topRight: Radius.circular(17.0),
+              ),
+              child: Image.network(
+                  'http://images.ctfassets.net/r7p9m4b1iqbp/NTJuSk4g0uaOwCGY8SwMq/228075a2bb20d88aff03466239628ed6/Natalie-Allen-12-Self-Portait-Japan.jpg?fit=fill&w=800&h=450'),
+            ),
+            Container(
+              height: 40,
+              child: Text(
+                "Title", 
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                )),
+            ),
+            SizedBox(
+                height: 60,
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.only(left:18.0, bottom: 10.0),
+                      width: 50.0,
+                      height: 50.0,
+                      decoration: new BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: new DecorationImage(
+                            fit: BoxFit.cover,
+                            image: new NetworkImage(
+                                'http://images.ctfassets.net/r7p9m4b1iqbp/NTJuSk4g0uaOwCGY8SwMq/228075a2bb20d88aff03466239628ed6/Natalie-Allen-12-Self-Portait-Japan.jpg?fit=fill&w=800&h=450')
+                          )
+                        ),
+                    ),
+                        /*
+                        child: Image.network(
+                            'http://images.ctfassets.net/r7p9m4b1iqbp/NTJuSk4g0uaOwCGY8SwMq/228075a2bb20d88aff03466239628ed6/Natalie-Allen-12-Self-Portait-Japan.jpg?fit=fill&w=800&h=450')),*/
+                    Text("Author"),
+                    Text("Time")
+                  ],
+                ))
+          ],
+        ));
   } // widget build
 } // class CardPosts
-
-//Image.asset('http://images.ctfassets.net/r7p9m4b1iqbp/NTJuSk4g0uaOwCGY8SwMq/228075a2bb20d88aff03466239628ed6/Natalie-Allen-12-Self-Portait-Japan.jpg?fit=fill&w=800&h=450'),
-/*shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(17.0),
-            ),*/
